c52d7d929034d2e2baf6491102f55b54754a3af0
mengyx-work <ymeng.ucla@gmail.com>
2016-09-07 22:20:39 +0000

    add mcc func

--- utils/validation_tools.py
+++ utils/validation_tools.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import numpy as np
 import os, sys, time
+from random import shuffle
+from sklearn.metrics import matthews_corrcoef
+
+
+## assuming the model output is the probability of being default,
+## then this probability can be used for ranking. Then using the fraction of
+## default in validation data to assign the proper threshold to the prediction
+def score_MCC(ground_truth, scores):
+    tmp_ground_truth = ground_truth
+    fault_frac = tmp_ground_truth.mean()
+    print 'score shape:', scores.shape, 
+    print 'mean of groud truth:', fault_frac
+    thres_value = np.percentile(scores, 100.*(1-fault_frac), axis=0)
+    print 'threshold value:', thres_value
+    binary_scores = scores > thres_value
+    binary_scores = binary_scores.astype(int)
+    ## convert to sk-learn format
+    np.place(binary_scores, binary_scores==0, -1)
+    np.place(tmp_ground_truth, tmp_ground_truth==0, -1)
+    #print ground_truth
+    #print binary_scores
+    return matthews_corrcoef(tmp_ground_truth, binary_scores)
 
 def create_validation_index(df, valid_frac = 0.2, dep_var_name = 'dep_var'):
   valid_index = []
