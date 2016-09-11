e8d587af5b738dce3c91e3b7547b92d9bfda510b
mengyx-work <ymeng.ucla@gmail.com>
2016-09-11 18:00:24 +0000

    update single et model

--- tests/single_et_model.py
+++ tests/single_et_model.py
@@ -7,7 +7,7 @@ import cPickle as pickle
 sys.path.append('/home/ymm/kaggle/xgboost_hyperopt')
 import utils
 from utils.models import RandomForestModel, ExtraTreeModel
-from utils.validation_tools import score_MCC, grid_search_cross_validate_model, list_const_params
+from utils.validation_tools import score_MCC, create_validation_index
 from utils.bosch_functions import load_processed_bosch_data
 
 models_yaml_file = 'trained_combined_model.yml'
@@ -21,11 +21,14 @@ data_yaml_file   = 'bosch_processed_data_dict.yml'
 
 train = load_processed_bosch_data(data_path, project_yml_path, data_yaml_file, data_index='0')
 print train.shape
-train_label = train[dep_var_name]
-tmp_train = train.copy()
-tmp_train.drop(dep_var_name, axis=1, inplace=True)
+train_index,valid_index = create_validation_index(train, 0.2, dep_var_name, True)  
+valid_data  = train.ix[valid_index]
+train       = train.ix[train_index]
+valid_label = valid_data[dep_var_name]
+
 
 model_params =  {'random_state' : 0, 'n_estimators' : 500, 'max_depth' : 4, 'criterion' : 'entropy', 'n_jobs' : -1}
 model = ExtraTreeModel(model_params)
-model.fit(tmp_train, train_label)
-res = model.predict(tmp_train)
+model.fit(train, dep_var_name)
+valid_result = model.predict(valid_data)
+print score_MCC(valid_label, valid_result)
