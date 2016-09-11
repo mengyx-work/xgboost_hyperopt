760ceefaf5b142891ce4ec0b79e8f6075832b61d
mengyx-work <ymeng.ucla@gmail.com>
2016-09-11 18:15:43 +0000

    add cv into combined model

--- tests/single_combined_model.py
+++ tests/single_combined_model.py
@@ -6,8 +6,8 @@ import cPickle as pickle
 
 sys.path.append('/home/ymm/kaggle/xgboost_hyperopt')
 from utils.bosch_functions import load_processed_bosch_data
-from utils.models import train_combined_model, predict_combined_model
-from utils.validation_tools import score_MCC, create_validation_index
+from utils.models import CombinedModel
+from utils.validation_tools import score_MCC, create_validation_index, cross_validate_model
 
 dep_var_name = 'Response'
 raw_models_yaml_file = 'raw_combined_models.yml'
@@ -21,15 +21,26 @@ data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
 data_yaml_file = 'bosch_processed_data_dict.yml'
 
 train = load_processed_bosch_data(data_path, project_yml_path, data_yaml_file, data_index='0')
-train_index,valid_index = create_validation_index(train, 0.2, dep_var_name, True)  
+train_index, valid_index = create_validation_index(train, 0.2, dep_var_name, True)  
 valid_data = train.ix[valid_index]
-train      = train.ix[train_index]
+tmp_train  = train.ix[train_index]
 
 ## train the comined model
-train_combined_model(train, dep_var_name, raw_models_yaml_file, project_path, trained_model_yaml_file)
+combined_model_params = {}
+combined_model_params['raw_models_yaml_file'] = raw_models_yaml_file
+combined_model_params['project_path'] = project_path
+combined_model_params['models_yaml_file'] = trained_model_yaml_file
 
-pred_df = predict_combined_model(valid_data, project_path, trained_model_yaml_file, score_MCC, dep_var_name)
+combined_model = CombinedModel(combined_model_params)
 
+'''
+## regular fit and predict
+combined_model.fit(tmp_train, dep_var_name)
+pred_df = combined_model.predict(valid_data)
 pred_df.to_csv('tmp.csv')
+'''
+
+results = cross_validate_model(train, dep_var_name, combined_model, score_MCC, 2)
+print results
 
 
