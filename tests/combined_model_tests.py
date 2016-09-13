69dbda4ba11561d36be72d82c64f0cb6a7aeadef
mengyx-work <ymeng.ucla@gmail.com>
2016-09-13 23:42:30 +0000

    add ingore

--- tests/combined_model_tests.py
+++ tests/combined_model_tests.py
@@ -18,9 +18,16 @@ project_path = '/mnt/home/ymm/kaggle/compete/current/combined_model'
 trained_model_yaml_file = 'trained_combined_model.yml'
 
 ## load training data
-project_yml_path = '/mnt/home/ymm/kaggle/compete/current'
-data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
-data_yaml_file = 'bosch_processed_data_dict.yml'
+## 15 bins data
+#project_yml_path = '/mnt/home/ymm/kaggle/compete/current'
+#data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
+#data_yaml_file = 'bosch_processed_data_dict.yml'
+
+## 5 bins data
+project_yml_path = '/home/ymm/kaggle/compete/current/model_6_bins_data'
+data_path = '/home/ymm/kaggle/bosch_data/bosch_complete_processed_6_bins_data'
+data_yaml_file = 'complete_subset_data_6_bins_dict.yml'
+
 train = load_processed_bosch_data(data_path, project_yml_path, data_yaml_file, data_index='0')
 
 ## train the comined model
@@ -33,21 +40,19 @@ combined_model = CombinedModel(combined_model_params)
 
 
 ############## Section of regular validation #######################
-
-train_index, valid_index = create_validation_index(train, 0.4, dep_var_name, True)  
+'''
+train_index, valid_index = create_validation_index(train, 0.5, dep_var_name, True)  
 valid_data = train.ix[valid_index]
 tmp_train  = train.ix[train_index]
 
 combined_model.fit(train, dep_var_name)
 pred_df = combined_model.predict(valid_data)
 print score_MCC(valid_data[dep_var_name], pred_df)
-
-
+'''
 
 ############## Section of using cross validation #######################
-
 ## cross-validate any combined model
-results = cross_validate_model(train, dep_var_name, combined_model, score_MCC, 2)
+results = cross_validate_model(train, dep_var_name, combined_model, score_MCC, 6)
 print results
 
 
