798bf47a7a3161932c5614f5e3fcbb1205bba1f9
mengyx-work <ymeng.ucla@gmail.com>
2016-09-13 22:06:30 +0000

    complete combined model test

--- tests/combined_model_tests.py
+++ tests/combined_model_tests.py
@@ -11,7 +11,7 @@ from utils.validation_tools import score_MCC, create_validation_index, cross_val
 
 dep_var_name = 'Response'
 
-## combined model information
+## params for combined model
 raw_models_yaml_file = 'raw_combined_models.yml'
 trained_model_yaml_file = 'trained_combined_model.yml'
 project_path = '/mnt/home/ymm/kaggle/compete/current/combined_model'
@@ -21,20 +21,30 @@ trained_model_yaml_file = 'trained_combined_model.yml'
 project_yml_path = '/mnt/home/ymm/kaggle/compete/current'
 data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
 data_yaml_file = 'bosch_processed_data_dict.yml'
-
 train = load_processed_bosch_data(data_path, project_yml_path, data_yaml_file, data_index='0')
-train_index, valid_index = create_validation_index(train, 0.4, dep_var_name, True)  
-valid_data = train.ix[valid_index]
-tmp_train  = train.ix[train_index]
 
 ## train the comined model
 combined_model_params = {}
 combined_model_params['raw_models_yaml_file'] = raw_models_yaml_file
 combined_model_params['project_path'] = project_path
 combined_model_params['models_yaml_file'] = trained_model_yaml_file
-
 ## build the combined model
 combined_model = CombinedModel(combined_model_params)
+
+
+############## Section of regular validation #######################
+
+train_index, valid_index = create_validation_index(train, 0.4, dep_var_name, True)  
+valid_data = train.ix[valid_index]
+tmp_train  = train.ix[train_index]
+
+combined_model.fit(train, dep_var_name)
+pred_df = combined_model.predict(valid_data)
+print score_MCC(valid_data[dep_var_name], pred_df)
+
+
+
+############## Section of using cross validation #######################
 
 ## cross-validate any combined model
 results = cross_validate_model(train, dep_var_name, combined_model, score_MCC, 2)
