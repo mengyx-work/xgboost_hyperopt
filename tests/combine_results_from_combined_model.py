236c7b46d85017f73075a9b7bdbe552089b0aa9a
mengyx-work <ymeng.ucla@gmail.com>
2016-09-17 05:47:28 +0000

    minor correction

--- tests/combine_results_from_combined_model.py
+++ tests/combine_results_from_combined_model.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('/home/ymm/kaggle/xgboost_hyperopt')
 from utils.validation_tools import combine_prediction_results_for_combined_models
 
-data_path               ='/home/ymm/kaggle/bosch/data_2_bins_cv_combined_models'
+data_path               ='/home/ymm/kaggle/bosch/data_2_bins_xgb_combined_models'
 submission_sample_file  = 'sample_submission.csv'
 submission_sample_path  = '/mnt/home/ymm/bosch'
 combine_prediction_results_for_combined_models(data_path, submission_sample_path, submission_sample_file, index_col_name = 'Id', res_col_name = 'Response') 
