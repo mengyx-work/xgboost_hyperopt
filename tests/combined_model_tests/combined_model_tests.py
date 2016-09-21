23a8da3699891056c5f73ec235321537ce357075
mengyx-work <ymeng.ucla@gmail.com>
2016-09-21 20:39:00 +0000

    restructure the combined model tests

--- tests/combined_model_tests/combined_model_tests.py
+++ tests/combined_model_tests/combined_model_tests.py
@@ -14,7 +14,6 @@ dep_var_name = 'Response'
 ## params for combined model
 raw_models_yaml_file    = 'raw_combined_models.yml'
 trained_model_yaml_file = 'trained_combined_model.yml'
-#project_path            = '/mnt/home/ymm/kaggle/compete/current/tmp'
 project_path            = './tmp'
 raw_models_yaml_path    = './'
 
@@ -22,13 +21,6 @@ raw_models_yaml_path    = './'
 project_yml_path = '/mnt/home/ymm/kaggle/compete/current'
 data_path = '/home/ymm/kaggle/bosch_data/bosch_complete_processed_15_bins_data'
 data_yaml_file = 'bosch_processed_data_dict.yml'
-
-'''
-## 5 bins data
-project_yml_path = '/home/ymm/kaggle/compete/current/model_6_bins_data'
-data_path = '/home/ymm/kaggle/bosch_data/bosch_complete_processed_6_bins_data'
-data_yaml_file = 'complete_subset_data_6_bins_dict.yml'
-'''
 
 train = load_processed_bosch_data(data_path, data_yaml_file, data_index='0', nrows=10000)
 
@@ -41,8 +33,8 @@ combined_model_params['models_yaml_file']       = trained_model_yaml_file
 ## build the combined model
 combined_model = CombinedModel(combined_model_params)
 
-
-##'''
+## warning! the multiple combined tests will overwite each ohter's results
+#'''
 ############## Section of regular validation #######################
 train_index, valid_index = create_validation_index(train, 0.5, dep_var_name, True)  
 valid_data = train.ix[valid_index]
@@ -54,12 +46,6 @@ print 'MCC score from validation: ', MCC(valid_data[dep_var_name], pred_df)
 #print score_MCC(valid_data[dep_var_name], pred_df)
 #'''
 
-############## Section of cross_valiate fit #######################
-combined_model.cross_vlidate_fit(train, dep_var_name)
-pred_df.label_name = dep_var_name
-pred_df = combined_model.predict(valid_data, score_conversion_type='A')
-print 'MCC score from cross_valiate_fit: ', MCC(valid_data[dep_var_name], pred_df)
-
 
 #'''
 ############## Section of using cross validation #######################
@@ -67,4 +53,12 @@ print 'MCC score from cross_valiate_fit: ', MCC(valid_data[dep_var_name], pred_d
 results = cross_validate_model(train, dep_var_name, combined_model, score_MCC, 3)
 print results
 #'''
+
+
+############## Section of cross_valiate fit #######################
+combined_model.cross_vlidate_fit(train, dep_var_name)
+pred_df = combined_model.predict(valid_data, score_conversion_type='A')
+print 'MCC score from cross_valiate_fit: ', MCC(valid_data[dep_var_name], pred_df)
+
+
 
